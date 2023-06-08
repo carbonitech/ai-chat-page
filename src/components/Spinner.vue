266b041e6cd77b3ce4a5559df1c8c890892fe056
carboni-joseph <joe@carbonitech.com>
2023-06-08 17:45:37 +0100

    add icon, add waiting spinner and clear prior resp

---
+++ src/components/Spinner.vue
@@ -0,0 +1,21 @@
+<script setup></script>
+<template>
+    <div class="loader"></div>
+</template>
+
+<style scoped>
+.loader {
+  margin: auto 0;
+  border: 2px solid #EAF0F6;
+  border-radius: 50%;
+  border-top: 2px solid #4caf50;
+  width: 20px;
+  height: 20px;
+  animation: spinner 4s linear infinite;
+}
+
+@keyframes spinner {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
+</style>
--- src/views/Chatbox.vue
+++ src/views/Chatbox.vue
@@ -1,10 +1,15 @@
 <script setup>
 import { ref } from 'vue'
+import Spinner from '@/components/Spinner.vue'
+import IconDocumentation from '@/components/icons/IconDocumentation.vue'
 
 const user_query = ref('')
 const chat_response = ref('')
+const waiting = ref(false)
 
 async function submitQuery() {
+    chat_response.value = ''
+    waiting.value = true
     try {
         const response = await fetch ('http://localhost:8000/chat', {
             method: 'POST',
@@ -18,12 +23,14 @@ async function submitQuery() {
             chat_response.value = response_data.response
         } else {
             console.log(response)
+            chat_response.value = 'Sorry, there was an error processing your request.'
             throw new Error('No response')
         }
     }
     catch (error) {
         console.error(error)
     }
+    waiting.value = false
 }
 </script>
 
@@ -34,10 +41,14 @@ async function submitQuery() {
             <button @click="submitQuery">Ask</button>
         </div>
         <div class="response">
-            <h2>Response</h2>
+            <div class="icon"><IconDocumentation /></div>
             <div v-if="chat_response">
                 <p> {{ chat_response }}</p>
             </div>
+            <div v-else-if="waiting">
+                <Spinner />
+            </div>
+            <div v-else>The AI's response will appear here ...</div>
         </div>
     </div>
 </template>
@@ -55,15 +66,18 @@ async function submitQuery() {
     display: flex;
     justify-content: space-between;
     align-items: baseline;
+    justify-self: flex-start;
+    overflow: visible;
 }
 
 input[type="text"] {
-  width: 100%;
-  padding: 10px;
-  margin: 10px;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  font-size: 16px;
+    width: 100%;
+    padding: 10px;
+    margin: 10px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    font-size: 14px;
+    overflow: visible;
 }
 
 button {
@@ -81,12 +95,17 @@ button:hover {
   background-color: #45a049;
 }
 
+.icon {
+    margin-right: 10px;
+}
 .response {
-  margin-top: 20px;
-  font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-  border-bottom: 1px solid #ccc;
-  padding: 5px;
-  border-radius: 4px;
-  background-color: #ffffff;
+    display: flex;
+    margin-top: 20px;
+    margin-bottom: 5px;
+    font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    border-bottom: 1px solid #ccc;
+    padding: 5px;
+    border-radius: 4px;
+    background-color: #ffffff;
 }
 </style>
